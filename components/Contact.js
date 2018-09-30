7a814312938000802c1bc1a02030f88ef08cee80
Przemyslaw Cygan <cyganprzemyslaw@gmail.com>
2018-09-30 13:32:06 +0200

    Change Contact.js, Add Contacts.js

--- components/Contact.js
+++ components/Contact.js
@@ -1,15 +1,21 @@
-var Contacts = React.createClass({
-  propTypes: {
-    items: React.PropTypes.array.isRequired,
+var Contact = React.createClass({
+  propTypes : {
+    item: React.PropTypes.object.isRequired, //czemu PropTypes, a nie propTypes
   },
 
   render: function() {
-    var contacts = this.props.items.map(function(contact) {
-        return React.createElement(Contact, {item: contact, key: contact.id});
-    });
-
     return (
-      React.createElement('ul', {className: 'contactsList'}, contacts)
-    );
-  }
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('img', {
+          className: 'contactImage',
+          src: 'https://www.stuff.tv/sites/stuff.tv/files/avatar.png'
+        }),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('p', {className: 'contactEmail', href: 'mailto:' + this.props.item.email}, 
+          this.props.item.email
+        )
+      )
+    )
+  },
 });
